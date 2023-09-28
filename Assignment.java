7ae3e23f838ec15b731895aba52db71980edd80a
Prerna Nagori <prernanagori5@gmail.com>
2023-09-28 20:04:30 +0530

    created Assignment file

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+public class Assignment {
+	public static void main(String[] args)
+	{
+       int a=2,b=1,c=1;
+		
+		a=a+b*c;
+		System.out.println(a);
+		a=a*b+c;
+		System.out.println(a);
+		a=a-b/c;
+		System.out.println(a);
+		a=a/b-c;
+		System.out.println(a);
+	}
+	
+
+}
